6d259d7d2041ba55d97d966087ee6331f77e8ffd
Arjun-R24 <arjunrajaram@wireless-10-105-169-253.umd.edu>
2024-01-28 16:10:13 -0500

    added recursive algo script

---
+++ recursive_algo_items/recursive-algo.py
@@ -0,0 +1,31 @@
+#recursive-algo
+import json 
+
+nodes = []
+edges = []
+
+with open('abc.json', 'r') as file:
+    data = json.load(file)
+
+# make sure algo adds things things to the array in the React flow format
+def add_node_and_edges(node, parent=None):
+    # Add the node to the nodes array
+    nodes.append({"id" : node["title"], "data" : {"label": node["title"]}, "position": {}})
+    
+    # If there is a parent, add an edge
+    if parent:
+        #edges.append((parent, node["title"]))
+        edges.append({"id" : parent + "-" + node["title"], "source" : parent, "target": node["title"], "type": "smoothstep"})
+
+    # Recursively add sub-nodes
+    if "sub-topics" in node and node["sub-topics"]:
+        for sub_node in node["sub-topics"]:
+            add_node_and_edges(sub_node, node["title"])
+
+# Start the recursion with the main topic
+add_node_and_edges(data)
+
+# Output the nodes and edges
+print("Nodes:", nodes)
+print("------------------------------------------")
+print("Edges:", edges)
