768c4db2f6a1bce6851a94b908f0e07d8fa26990
eg337 <eg6322@gmail.com>
2024-02-04 18:42:31 -0500

    staging for merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,47 +8,4 @@ function App() {
   )
 }
 
-<<<<<<< HEAD
 export default App
-
-
-// // import React from 'react';
-// // import HelloWorld from 'src/HelloWorld';
-
-// // function App() {
-// //   return (
-// //     <div>
-// //       <HelloWorld />
-// //     </div>
-// //   );
-// // }
-
-// // export default App;
-
-
-// import React, { useState, useEffect } from 'react';
-// import './App.css';
-
-// function App() {
-//   const [currentTime, setCurrentTime] = useState(0);
-//   const [currentDate, setCurrentDate] = useState(0);
-//   useEffect(() => {
-//   fetch(' http://127.0.0.1:8000/').then(res => res.json()).then(data => {
-//       setCurrentTime(data.time);
-//       setCurrentDate(data.date)
-//     });
-//   }, []);
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//       <p>The date is {currentDate} and the time is {currentTime}.</p> <br/>
-
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-=======
-export default App
->>>>>>> bd18eb1e9a1729faa2a6c68e0d983104f348ebd1
